2c50cfb7be3b5079a1c2cc62860ce4cc3650ff86
PabloQuadros <pablo.aquadros@gmail.com>
2023-03-08 14:25:29 -0300

    FindMaximumAPI

---
+++ exercises/FindMaximumAPI/Server/FindMaxServiceImpl.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using Max;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Max.FindMaxService;
+
+namespace Server
+{
+    public class FindMaxServiceImpl : FindMaxServiceBase
+    {
+        public override async Task findMaximum(IAsyncStreamReader<FindMaxRequest> requestStream, IServerStreamWriter<FindMaxResponse> responseStream, ServerCallContext context)
+        {
+            int? max = null;
+            
+            while (await requestStream.MoveNext())
+            {
+                if (max  == null || max < requestStream.Current.Num )
+                {
+                    max = requestStream.Current.Num;
+                    await responseStream.WriteAsync(new FindMaxResponse() { Max = max.Value });
+                }
+            }
+        }
+    }
+}
